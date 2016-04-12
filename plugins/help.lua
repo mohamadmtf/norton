867ea6126470a80d1314087cb2b20c3e86c29d2b
mohamadmtf <mobilebaran2013@yahoo.com>
2016-04-12 10:44:23 +0430

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -3,93 +3,89 @@ do
 function run(msg, matches)
   return '🚀 Fun Time'.. [[
 
+🔘Fun Time:🎯
 
-⚡!sh or !uptime
-«دریافت مشخصات سی پی یو ربات»
-⚡!wiki {search}
-«سرچ مطلبی در ویکی پدیا»
-⚡!imdb {movie}
-«دریافت اطلاعات فیلم مورد نظر»
-⚡!git {github user}
+🔹!git {github user}
 «دریافت اطلاعات یک یوزر در گیت هاب»
-⚡!google 
+🔸!google 
 «سرچ کردن در گوگل»
-⚡!aparat
+🔹!aparat
 «سرچ کردن یک ویدیو در اپارات»
-⚡!reply {txt}
+🔸!reply {txt}
 «تکرار یک حرف توسط ربات»
-⚡!gif {txt}
+🔹!gif {txt}
 «سرچ کردن یک متن و دریافت ان به صورت گیف»
-⚡!gps {city} {country}
-«دریافت عکس و نقشه از مکان مورد نظر در نقشه»
-⚡!insta {instagram user}
+🔸!gps {city} {country}
+«دریافت عکس و نقشه از مکان مورد نظر در گوگل مپ»
+🔹!insta {instagram user}
 «سرچ کردن یک اکانت عمومی در اینستاگرام»
-⚡!qr {link|txt}
+🔸!qr {link|txt}
 «دریافت هر چیز به صورت بارکد»
-⚡!calc {2+7}
+🔹!calc {2+7}
 «محاسبه عملیات ریاضی»
-⚡!weather {city}
+🔸!weather {city}
 «دریافت وضعیت اب و هوای یک شهر»
-⚡!t2i 
+🔹!t2i 
 «تبدیل متن به عکس»
-⚡!tophoto
-«تبدیل استیکر به عکس»
-⚡!web {http://.......}
+🔹!webshot {http://.......}
 «فرستادن اسکرین از پیج مورد نظر»
-⚡!isup
-«دریافت ای پی و پینگ»
-⚡!insudo
+🔹sudo | manager
 «نشان دادن سودو اصلی»
-⚡!time
+🔸!time
 «نشان دادن ساعت»
-⚡!voice {txt}
-«تبدیل متن به صدا توسط ربات»
-〰〰〰〰〰〰〰〰〰〰〰
-📧 Service Provider : 🔧
+🔹!voice {txt}
+«تبدیل متن به صدا توسط ربات» 
+🔶!wikifa {search}
+«سرچ مطلبی در ویکی پدیا»
+🔷!imdb {movie}
+«دریافت اطلاعات فیلم مورد نظر»
+➖➖➖➖➖➖➖➖➖➖➖
+🔘Service Provider:🔧
 
-⚡!link
+🔸!link
 «نمایش لینک گروه»
-⚡!newlink
+🔹!newlink
 «ایجاد لینک جدید»
-⚡!linkpv
+🔸!linkpv
 «فرستادن لینک گروه در پی وی»
-⚡!invite {@username}
+🔹!invite {@username}
 «اضافه کردن شخص تو گروه(حتما شماره ربات را سیو کرده باشد)»
-〰〰〰〰〰〰〰〰〰〰〰
-💡 Member Profiles & Groups : 🔰
+➖➖➖➖➖➖➖➖➖➖➖
+🔘️Member Profiles & Groups:🔰
 
-⚡!owner
+🔸!owner
 «دریافت ایدی مدیر اصلی سوپر گروه»
-⚡!id
+🔹!id
 «دریافت ایدی سوپر گروه یا خود و ...»
-⚡!res {@username}
+🔸!res {@username}
 «پیدا کردن ای دی شخص»
-⚡!info
+🔹!info
 «دریافت اطلاعات خود یا دیگران»
-〰〰〰〰〰〰〰〰〰〰〰
-🔗 ♣️Filter Word Mode : ♻️
+➖➖➖➖➖➖➖➖➖➖➖
+🔘️Filter Word Mode:♻️
 
-⚡!filter + {word}
+🔸!filter + {word}
 «اضافه کردن یک کلمه به لیست کلمات فیلتر شده»
-⚡!filter - {word}
+🔹!filter - {word}
 «حذف یک کلمه از لیست کلمات فیلتر شده»
-⚡!filterlist
+🔸!filterlist
 «دریافت لیست کلمات فیلتر شده»
-〰〰〰〰〰〰〰〰〰〰〰
-♠️🔆 Pray : 
+➖➖➖➖➖➖➖➖➖➖➖
+🔘Pray:🔆
 
-⚡!praytime
+🔹!praytime
 «دریافت وقت اذان یک شهر»
-〰〰〰〰〰〰〰〰〰〰
-💢 Support : 
+➖➖➖➖➖➖➖➖➖➖➖
+🔘Support:💠
 
-⚡!feedback {idea}
+🔸!feedback {idea}
 «ارسال نظرات به سازنده ربات»
-⚡!version
+🔹!version
 «دریافت ورژن ربات»
-⚡!tosupport
-«دریافت لینک ساپورت ربات»
-⚡!share 
+🔸!adddeveloper | !addmanager
+«دعوت سازنده یا مدیر ربات به گروه»
+(در صورت مشکل)
+🔹!share 
 «فرستادن شماره ربات»
 _____________________________
 Have a good groups with ultimate security bot 📡
